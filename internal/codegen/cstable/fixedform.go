6cbee34c786b875aa372cd3fb8806f1a7ccac6f2
Glenn Fiedler <glenn@networknext.com>
2026-09-09 21:44:00 -0400

    cstable, compiler: modernize -fix

--- internal/codegen/cstable/fixedform.go
+++ internal/codegen/cstable/fixedform.go
@@ -235,7 +235,7 @@ func appendFixedU32(b []byte, v uint32) []byte {
 }
 
 func appendFixedU64(b []byte, v uint64) []byte {
-	for i := 0; i < 8; i++ {
+	for i := range 8 {
 		b = append(b, byte(v>>(8*i)))
 	}
 	return b
@@ -1576,10 +1576,7 @@ func (g *tableGen) emitFixedRoot(st *ir.Struct) {
 
 func (g *tableGen) emitCsByteArray(b []byte) {
 	for i := 0; i < len(b); i += 16 {
-		end := i + 16
-		if end > len(b) {
-			end = len(b)
-		}
+		end := min(i+16, len(b))
 		var sb strings.Builder
 		sb.WriteString("   ")
 		for _, v := range b[i:end] {
